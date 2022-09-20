c818619679e667ea6547e85c7cff95540432f60a
Iqtidar102011 <iqidarwasif@gmail.comm>
2022-09-20 13:22:20 +0200

    load data dynamically from API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
+
+function ExternalUsers() {
+  const [users, Setusers] = useState([])
+  // useEffect(()=>{},[])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => Setusers(data))
+  }, [])
+  return (
+    <div>
+      <h1>Total Users:{users.length}</h1>
+      {/* usimg map to create component(User) and show data  */}
+      {
+        users.map(user => <User key={user.id} name={user.name} mail={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props) {
+  return (
+    <div style={{ border: '2px solid black' }}>
+      <h3>Name:{props.name}</h3>
+      <p>Email: {props.mail}</p>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
 function Counter() {
   const [count, setCount] = useState(0);
   // useState will giva an array of two element ,initial value and a function
